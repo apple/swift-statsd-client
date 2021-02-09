0765b71586fc3952d2feb0700a9cfb89c6a764dc
tomer doron <tomer@apple.com>
2021-02-09 11:02:56 -0800

    use welcoming language (#28)

--- scripts/soundness.sh
+++ scripts/soundness.sh
@@ -16,6 +16,22 @@
 set -eu
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+printf "=> Checking for unacceptable language... "
+# This greps for unacceptable terminology. The square bracket[s] are so that
+# "git grep" doesn't find the lines that greps :).
+unacceptable_terms=(
+    -e blacklis[t]
+    -e whitelis[t]
+    -e slav[e]
+    -e sanit[y]
+)
+if git grep --color=never -i "${unacceptable_terms[@]}" > /dev/null; then
+    printf "\033[0;31mUnacceptable language found.\033[0m\n"
+    git grep -i "${unacceptable_terms[@]}"
+    exit 1
+fi
+printf "\033[0;32mokay.\033[0m\n"
+
 printf "=> Checking linux tests... "
 FIRST_OUT="$(git status --porcelain)"
 ruby "$here/../scripts/generate_linux_tests.rb" > /dev/null
@@ -41,7 +57,7 @@ else
 fi
 
 printf "=> Checking license headers\n"
-tmp=$(mktemp /tmp/.swift-statsd-client-sanity_XXXXXX)
+tmp=$(mktemp /tmp/.swift-statsd-client-soundness_XXXXXX)
 
 for language in swift-or-c bash dtrace; do
   printf "   * $language... "
