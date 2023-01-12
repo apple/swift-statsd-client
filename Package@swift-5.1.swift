56958b9cb167b7fae9f472dba37732f921fd07eb
Romain Pouclet <rpouclet@apple.com>
2023-01-12 11:23:08 -0800

    Change range for Swift < 5.2

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", "2.0.0" ..< "2.29.0"),
     ],
     targets: [
         .target(name: "StatsdClient", dependencies: ["CoreMetrics", "NIO"]),
